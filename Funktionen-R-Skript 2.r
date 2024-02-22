999fd0b5de204e34568ba0319490618c397f7512
Aigul99 <124449172+Aigul99@users.noreply.github.com>
2024-02-22 23:35:07 +0100

    update Helfer-Funktionen

--- Funktionen-R-Skript 2.r
+++ Funktionen-R-Skript 2.r
@@ -1,10 +1,6 @@
-# Funktion zur Behandlung von fehlenden Werten durch Mittelwert-Ersatz
-handle_missing_values <- function(data) {
-  for (col in colnames(data)) {
-    if (anyNA(data[[col]])) {  # Überprüfen, ob die Spalte fehlende Werte enthält
-      mean_value <- mean(data[[col]], na.rm = TRUE)  # Berechnung des Mittelwerts ohne NA
-      data[[col]][is.na(data[[col]])] <- mean_value  # Ersetzen der fehlenden Werte durch den Mittelwert
-    }
+# Hilfsfunktion zur Überprüfung, ob eine Variable numerisch ist
+check_numeric <- function(df, var) {
+  if (!is.numeric(df[[var]])) {
+    stop(paste("Die Variable", var, "ist nicht numerisch. Bitte wählen Sie eine numerische Variable."))
   }
-  return(data)
 }
