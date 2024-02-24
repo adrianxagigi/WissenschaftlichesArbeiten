d8e535e87d7a517e3a13dfb7af637f9fb539a97a
Aigul99 <124449172+Aigul99@users.noreply.github.com>
2024-02-24 20:20:03 +0100

    Update

--- Funktionen-R-Skript 2.r
+++
@@ -1,6 +0,0 @@
-# Hilfsfunktion zur Überprüfung, ob eine Variable numerisch ist
-check_numeric <- function(df, var) {
-  if (!is.numeric(df[[var]])) {
-    stop(paste("Die Variable", var, "ist nicht numerisch. Bitte wählen Sie eine numerische Variable."))
-  }
-}
